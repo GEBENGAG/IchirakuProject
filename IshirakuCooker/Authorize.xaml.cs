58384d36eebb344146340656add4019cf9e8bcae
Yugen <yugensochi89@gmail.com>
2023-05-12 12:51:23 +0300

    authorize window added

---
+++ IshirakuCooker/Authorize.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IshirakuCooker
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorize.xaml
+    /// </summary>
+    public partial class Authorize : Page
+    {
+        public Authorize()
+        {
+            InitializeComponent();
+        }
+
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        { 
+            MainWindow main = new MainWindow();
+            main.MainFrame.Content = null;
+            main.MainFrame.IsEnabled = false;
+
+        }
+    }
+}
--- IshirakuCooker/MainWindow.xaml.cs
+++ IshirakuCooker/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,20 +21,36 @@ namespace IshirakuCooker
     /// </summary>
     public partial class MainWindow : Window
     {
-        ichirakuEntities entities = new ichirakuEntities();
+        
+        static ichirakuEntities entities = new ichirakuEntities();
+        static int status = entities.СтатусЗаказа.Find(1).ИдСтатусаЗаказа;
+
         public MainWindow()
         {
+            
+            
+
+        
             InitializeComponent();
-            OrdersGrid.ItemsSource = entities.Заказ.ToList();
+            MainFrame.Content = new Authorize();
+            OrdersGrid.ItemsSource = entities.Заказ.Where(k=>k.СтатусЗаказа1.ИдСтатусаЗаказа.Equals(status)).ToList();
              
             
         }
-
         private void OrderStatusChange_Click(object sender, RoutedEventArgs e)
         {
+            
             int id = ((IshirakuCooker.Заказ)OrdersGrid.CurrentItem).ИдЗаказа;
-            entities.Заказ.Find(id).СтатусЗаказа1 = entities.СтатусЗаказа.Find(2); 
-             
+            entities.Заказ.Find(id).СтатусЗаказа1 = entities.СтатусЗаказа.Find(3);
+            entities.SaveChanges();
+            OrdersGrid.ItemsSource = entities.Заказ.Where(k=>k.СтатусЗаказа1.ИдСтатусаЗаказа.Equals(status)).ToList();
+
+        }
+
+        private void updateGrid_Click(object sender, RoutedEventArgs e)
+        {
+            OrdersGrid.ItemsSource = entities.Заказ.Where(k => k.СтатусЗаказа1.ИдСтатусаЗаказа.Equals(status)).ToList();
+
         }
     }
 }
